27fe8dcbfb4d5c37af41399cdeb77d7edaf0610b
UnsignedArduino <UnsignedArduino@outlook.com>
2022-12-23 21:09:01 -0500

    Program can accept arguments

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,41 @@
-console.log("Hello, world!");
+import yargs, { string } from "yargs";
+import { hideBin } from "yargs/helpers";
+
+interface ProgramArguments {
+  type: "example" | "community";
+  key: string;
+  name: string;
+  author: string;
+  githubProfileURL: string | undefined;
+  originalPostURL: string | undefined;
+  // description: string | undefined;
+  // save: string;
+}
+
+const args: ProgramArguments = yargs(hideBin(process.argv))
+  .options({
+    type: {
+      choices: ["example", "community"] as const,
+      alias: ["t"],
+      demandOption: true,
+    },
+    key: { type: "string", alias: ["k"], demandOption: true },
+    name: { type: "string", alias: ["n"], demandOption: true },
+    author: { type: "string", alias: ["a"], demandOption: true },
+    githubProfileURL: { type: "string" },
+    originalPostURL: { type: "string" },
+    // description: { type: "string", alias: ["d"] },
+    // save: { type: "string", alias: ["s"], demandOption: true },
+  })
+  .parseSync();
+
+console.log(
+  `Arguments received: ${(() => {
+    let result = "{";
+    for (const [key, value] of Object.entries(args)) {
+      result += ` ${key}: ${value} `;
+    }
+    result += "}";
+    return result;
+  })()}`
+);
