f024e60bea15ecf47c8254bade4974701f20f11f
UnsignedArduino <UnsignedArduino@outlook.com>
2023-03-13 17:48:09 -0400

    Redo helper script

--- src/main.ts
+++ src/main.ts
@@ -1,105 +1,103 @@
-import yargs, { boolean, string } from "yargs";
-import { hideBin } from "yargs/helpers";
 import fs from "fs";
 import path from "path";
-import { checkPaths, createEmptyFiles, createDirectory } from "./pathing";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-interface ProgramArguments {
-  type: "example" | "community";
-  key: string;
-  name: string;
-  author: string;
-  githubProfileURL: string | undefined;
-  originalPostURL: string | undefined;
-  // description: string | undefined;
-  // save: string;
-  force: boolean | undefined;
-}
-
-const args: ProgramArguments = yargs(hideBin(process.argv))
-  .options({
-    type: {
-      choices: ["example", "community"] as const,
-      alias: ["t"],
-      demandOption: true,
+yargs(hideBin(process.argv))
+  .command(
+    "add <key>",
+    "Helper to add an example, community circuit, or blog post to the Community repository.",
+    {
+      type: {
+        alias: "t",
+        desc: "The type of thing to add.",
+        choices: ["examples", "community"] as const,
+        demandOption: true,
+      },
+      name: {
+        alias: "n",
+        desc: "The title of the thing to add.",
+        type: "string",
+        demandOption: true,
+      },
+      author: {
+        alias: "a",
+        desc: "The author of the thing.",
+        type: "string",
+        demandOption: true,
+      },
+      githubProfileURL: {
+        desc: "The GitHub profile URL for the author.",
+        type: "string",
+      },
+      originalPostURL: {
+        desc: "The original post URL for example or community circuit.",
+        type: "string",
+      },
     },
-    key: { type: "string", alias: ["k"], demandOption: true },
-    name: { type: "string", alias: ["n"], demandOption: true },
-    author: { type: "string", alias: ["a"], demandOption: true },
-    githubProfileURL: { type: "string" },
-    originalPostURL: { type: "string" },
-    // description: { type: "string", alias: ["d"] },
-    // save: { type: "string", alias: ["s"], demandOption: true },
-    force: { type: "boolean", alias: ["f"] },
-  })
-  .parseSync();
+    (args) => {
+      const cwd = process.cwd();
+      const typeDirPath = path.join(cwd, "circuits", args.type);
+      const allJSONPath = path.join(typeDirPath, "all.json");
+      const newSavePath = path.join(typeDirPath, "saves", args.key + ".txt");
+      const metadataDirPath = path.join(typeDirPath, "metadata", args.key + "");
+      const infoPath = path.join(metadataDirPath, "info.json");
+      const descriptionPath = path.join(metadataDirPath, "description.html");
 
-console.log(
-  `Arguments received: ${(() => {
-    let result = "{";
-    for (const [key, value] of Object.entries(args)) {
-      result += ` ${key}: ${value} `;
+      for (const path of [
+        { path: allJSONPath, mustExist: true },
+        { path: newSavePath, mustExist: false },
+        { path: metadataDirPath, mustExist: false },
+      ]) {
+        if (path.mustExist) {
+          if (!fs.existsSync(path.path)) {
+            console.error(`${path.path} does not exist!`);
+            process.exit(1);
+          }
+        } else {
+          if (fs.existsSync(path.path)) {
+            console.error(`${path.path} already exists!`);
+            process.exit(1);
+          }
+        }
+      }
+
+      fs.mkdirSync(metadataDirPath);
+
+      for (const path of [newSavePath, descriptionPath]) {
+        fs.writeFileSync(path, "");
+      }
+
+      const all = JSON.parse(
+        fs.readFileSync(allJSONPath, { encoding: "utf8" })
+      );
+      if (all.allSaves[args.name + ""] != undefined) {
+        console.error(`Key ${args.name} already exists!`);
+        process.exit(1);
+      }
+
+      all.allSaves[args.name + ""] = args.key;
+      fs.writeFileSync(allJSONPath, JSON.stringify(all, null, 2) + "\n", {
+        encoding: "utf8",
+      });
+
+      const info = {
+        author: {
+          name: args.author,
+          githubProfileURL: args.githubProfileURL,
+          originalPostURL: args.originalPostURL,
+        },
+        save: {
+          title: args.name,
+        },
+      };
+      fs.writeFileSync(infoPath, JSON.stringify(info, null, 2) + "\n", {
+        encoding: "utf8",
+      });
+
+      console.info(`Copy the description to ${descriptionPath}`);
+      console.info(`Copy the save code to ${newSavePath}`);
     }
-    result += "}";
-    return result;
-  })()}`
-);
-
-const cwd = process.cwd();
-console.log(`Current working directory: ${cwd}`);
-
-const typeDirPath = path.join(cwd, "circuits", args.type);
-const allJSONPath = path.join(typeDirPath, "all.json");
-const newSavePath = path.join(typeDirPath, "saves", args.key + ".txt");
-const metadataDirPath = path.join(typeDirPath, "metadata", args.key);
-const infoPath = path.join(metadataDirPath, "info.json");
-const descriptionPath = path.join(metadataDirPath, "description.html");
-
-checkPaths(
-  [
-    { path: allJSONPath, mustExist: true },
-    { path: newSavePath, mustExist: false },
-    { path: metadataDirPath, mustExist: false },
-  ],
-  args.force
-);
-
-createDirectory(metadataDirPath, args.force);
-createEmptyFiles([newSavePath, descriptionPath]);
-
-console.log(`Parsing JSON from ${allJSONPath}`);
-const all = JSON.parse(fs.readFileSync(allJSONPath, { encoding: "utf8" }));
-if (all.allSaves[args.name] != undefined) {
-  if (args.force) {
-    console.warn(`Key ${args.name} already exists, overwriting!`);
-  } else {
-    console.error(`Key ${args.name} already exists!`);
-    process.exit(1);
-  }
-}
-all.allSaves[args.name] = args.key;
-console.log(`Writing new JSON to ${allJSONPath}`);
-fs.writeFileSync(allJSONPath, JSON.stringify(all, null, 2) + "\n", {
-  encoding: "utf8",
-});
-
-const info = {
-  author: {
-    name: args.author,
-    githubProfileURL: args.githubProfileURL,
-    originalPostURL: args.originalPostURL,
-  },
-  save: {
-    title: args.name,
-  },
-};
-console.log(`Writing info to ${infoPath}`);
-fs.writeFileSync(infoPath, JSON.stringify(info, null, 2) + "\n", {
-  encoding: "utf8",
-});
-
-console.info("Done!");
-console.info(`Copy the description to ${descriptionPath}`);
-console.info(`Copy the save code to ${newSavePath}`);
-
-process.exit(0);
+  )
+  .help()
+  .parseSync();
